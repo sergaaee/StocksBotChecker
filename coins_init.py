9aaac30de193975821f06b6ea8bc6177ae9044f7
sergaaee <sergoiwj1@gmail.com>
2021-12-26 20:43:16 +0200

    better rounding

--- coins_init.py
+++ coins_init.py
@@ -1,8 +1,5 @@
 import requests
 
-url = 'https://ftx.com/api/markets'
-r = requests.get(url)
-result = r.json()['result']
 
 
 
@@ -34,17 +31,21 @@ class coin:
     def change24h(self):
         return str(self._change24h) + "%"
 
+url = 'https://ftx.com/api/markets'
+r = requests.get(url)
+result = r.json()['result']
 
-ton = coin(baseCurrency=result[570]['baseCurrency'], quoteCurrency=result[570]['quoteCurrency'], price=result[570]['price'], change1h=round(result[570]['change1h'], 3)*100, change24h=round(result[570]['change24h'], 3)*100)
+
+ton = coin(baseCurrency=result[570]['baseCurrency'], quoteCurrency=result[570]['quoteCurrency'], price=result[570]['price'], change1h=round(result[570]['change1h'], 4)*100, change24h=round(result[570]['change24h'], 4)*100)
 print(ton.baseCurrency, ton.quoteCurrency, ton.price, ton.change1h, ton.change24h)
 
-btc = coin(baseCurrency=result[150]['baseCurrency'], quoteCurrency=result[150]['quoteCurrency'], price=result[150]['price'], change1h=round(result[150]['change1h'], 3)*100, change24h=round(result[150]['change24h'], 3)*100)
+btc = coin(baseCurrency=result[150]['baseCurrency'], quoteCurrency=result[150]['quoteCurrency'], price=result[150]['price'], change1h=round(result[150]['change1h'], 4)*100, change24h=round(result[150]['change24h'], 4)*100)
 print(btc.baseCurrency, btc.quoteCurrency, btc.price, btc.change1h, btc.change24h)
 
-eth = coin(baseCurrency=result[260]['baseCurrency'], quoteCurrency=result[260]['quoteCurrency'], price=result[260]['price'], change1h=round(result[260]['change1h'], 3)*100, change24h=round(result[260]['change24h'], 3)*100)
+eth = coin(baseCurrency=result[260]['baseCurrency'], quoteCurrency=result[260]['quoteCurrency'], price=result[260]['price'], change1h=round(result[260]['change1h'], 4)*100, change24h=round(result[260]['change24h'], 4)*100)
 print(eth.baseCurrency, eth.quoteCurrency, eth.price, eth.change1h, eth.change24h)
 
-pos=0
+#pos=0
 #for answer in result:
     #pos += 1
     #print(answer['name'])
